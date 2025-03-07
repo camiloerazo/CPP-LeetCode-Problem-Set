8ddc976288093dd4da40feccc44ab4df14de9169
camiloerazo <camiloerazo@javerianacali.edu.co>
2025-03-07 10:00:37 -0500

    Solved twoSum problem beating 100% of the solutions using a hashtable

--- 1.twoSum.cpp
+++ 1.twoSum.cpp
@@ -4,21 +4,41 @@
 
 using namespace std;
 
+void pm(unordered_map<int, int> m){
+    for (const auto& elem : m) cout << elem.first << endl;
+}
+
 vector<int> twoSum(vector<int>& nums, int target){
     const int l = nums.size();
     unordered_map<int, int> m;
-    for (int i = 0; i < l; i++){
+    m[abs(nums[0] - target)] = 0;
+    for (int i = 1; i < l; i++){
         cout << "Entered the loop with element = " << nums[i] << endl;
-        
+        //Aproach using absolute value
+        //First step: check if the nums[i] is not on the map
+        auto it = m.find(abs(nums[i]));
+        if (it != m.end()){ //The key exist
+            //cout << "They key exist " << endl;
+            const auto key = it -> first;
+            //cout << "This is the key = " << key << endl;
+            const auto value = it -> second;
+            if (nums[i] + nums[value] == target){
+                return {value, i};
+            }else{
+                m[abs(nums[i] - target)] = i;
+            }
+        }else{ //The key does not exist
+            m[abs(nums[i] - target)] = i;
+            cout << "Added " << abs(nums[i] - target) << " to the map" << endl;
+        }
     }
-    for (const auto& pair : m){
-        cout << pair.first << endl;
-    }
+    //pm(m);
     return {};
 }
 
 int main(){
-    vector<int> v = {8,2,3,6,10,5}; // [2, 3, 5, 6, 8, 10]
-    twoSum(v, 7);
+    vector<int> v = {-11,7,3,2,1,7,-10,11,21,3}; // [2, 3, 5, 6, 8, 10]
+    vector<int> r = twoSum(v, 11);
+    for (const auto& elem : r) cout << elem << endl;
     return 0;
 }
