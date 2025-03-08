a3860f1c7c595ac1d227b87feff8c966436a6d4a
camiloerazo <juancamiloerazo82@gmail.com>
2025-03-07 20:19:49 -0500

    Added problem to solve later

---
+++ 384. Shuffle an Array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+        Solution(vector<int>& nums) {
+
+        }
+        
+        vector<int> reset() {
+            return {};
+        }
+        
+        vector<int> shuffle() {
+            return {};
+        }
+};
+    
+
+int main(){
+
+    return 1;
+}
