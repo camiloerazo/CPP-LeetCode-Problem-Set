702cf211b07c075437f69eaf6559546e8b227084
camiloerazo <juancamiloerazo82@gmail.com>
2025-03-31 21:14:37 -0500

    partial solution to the problem

---
+++ 19.Remove nth node.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+    
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+        while(n > 0){
+
+        }
+}
+
+int main(){
+    cout << "hola" << endl;
+    return 1;
+}
