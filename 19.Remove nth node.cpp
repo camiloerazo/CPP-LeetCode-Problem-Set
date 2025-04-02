411466af64c28ff6c519506a992616022f843c6c
camiloerazo <juancamiloerazo82@gmail.com>
2025-04-02 09:54:49 -0500

    solution created

--- 19.Remove nth node.cpp
+++ 19.Remove nth node.cpp
@@ -20,14 +20,48 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
     
 ListNode* removeNthFromEnd(ListNode* head, int n) {
-        while(n > 0){
-
-        }
+    vector<ListNode*> data;
+    ListNode* dummy = head;
+    while(1){
+        //cout << dummy->val << endl;
+        data.push_back(dummy);
+        if (dummy -> next != nullptr){
+            dummy = dummy->next;
+        }else break;
+    }
+    n = data.size() - n - 1;
+    if (n < 0){
+        head = head -> next;
+    }
+    else if(n == data.size() - 2){
+        data[data.size() - 2]->next = nullptr;
+    }else{
+        data[n]->next = data[n+2];
+    }
+    while(1){
+        cout << head->val << endl;
+        data.push_back(head);
+        if (head -> next != nullptr){
+            head = head->next;
+        }else break;
+    }
+    return head;
 }
 
 int main(){
-    cout << "hola" << endl;
+    ListNode head(12);
+    ListNode node1(3);
+    ListNode node2(7);
+    ListNode node3(10);
+    ListNode node4(2);
+    head.next = &node1;
+    node1.next = &node2;
+    node2.next = &node3;
+    node3.next = &node4;
+    ListNode* test  = &head;
+    removeNthFromEnd(test,1);
     return 1;
 }
